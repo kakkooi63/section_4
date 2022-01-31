07f338dfa90d14732690a8b290f68c3e8868dc08
kakkooi63 <kakkooi63@gmail.com>
2022-01-31 11:18:25 +0700

    section 4

--- lib/main.dart
+++ lib/main.dart
@@ -4,50 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int nilai = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Incremental Apps"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "$nilai",
-                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dialog"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => Dialog(
+                child: Text("halo"),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        nilai--;
-                        print(nilai);
-                        setState(() {});
-                      },
-                      child: Icon(Icons.remove)),
-                  ElevatedButton(
-                      onPressed: () {
-                        nilai++;
-                        print(nilai);
-                        setState(() {});
-                      },
-                      child: Icon(Icons.add)),
-                ],
-              )
-            ],
-          ),
+            );
+          },
+          child: Text("Show Dialog"),
         ),
       ),
     );
