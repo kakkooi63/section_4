f49edf023e99402ea1eafdeb7eb0b1fa2e78b000
kakkooi63 <kakkooi63@gmail.com>
2022-01-31 16:30:45 +0700

    text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,19 +18,88 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  bool Ishidden = true;
+
+  TextEditingController emailc = TextEditingController();
+  TextEditingController passc = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Text Field")),
-      body: Center(
-        child: Padding(
+        appBar: AppBar(title: Text("Aplikasi terbaru")),
+        body: Padding(
           padding: const EdgeInsets.all(30),
-          child: TextField(),
-        ),
-      ),
-    );
+          child: ListView(
+            padding: EdgeInsets.all(20),
+            children: [
+              TextField(
+                controller: emailc,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email_outlined),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                  label: Text(
+                    "Email",
+                    style: TextStyle(),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: passc,
+                autocorrect: false,
+                obscureText: Ishidden,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.vpn_key),
+                  suffix: IconButton(
+                    onPressed: () {
+                      if (Ishidden == true) {
+                        Ishidden = false;
+                      } else {
+                        Ishidden = true;
+                      }
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.remove_red_eye),
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                  label: Text("Password"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("Email ${emailc.text}dan Password ${passc.text}");
+                },
+                child: Text("Login"),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+              )
+            ],
+          ),
+        ));
   }
 }
